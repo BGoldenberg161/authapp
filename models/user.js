84fff232583c6dd14f82e7c24e4f6b4a4a4d6a7b
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-08-13 13:16:02 -0600

    [auth]7 passing

---
+++ models/user.js
@@ -0,0 +1,69 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 & 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password must be between 8 & 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  user.addHook('beforeCreate', function(pendingUser){
+    // has password
+    let hash = bcrypt.hashSync(pendingUser.password, 12)
+    //set password to the hash
+    pendingUser.password = hash
+  })
+
+  user.prototype.validPassword = function(passwordTyped){
+    let correctPassword = bcrypt.compareSync(passwordTyped, this.password)
+    // return true or false based on correct password
+    return correctPassword
+  }
+  // remove the password before it gets serialized
+  user.prototype.toJSON = function() {
+    let userData = this.get()
+    delete userData.password
+    return userData
+  }
+
+  return user;
+};
